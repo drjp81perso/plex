67602a9739de3b4e04d7d329b1fe6d6649c73113
Jean-Paul Lizotte <jean-paul.lizotte@emyode.com>
2023-12-27 20:45:11 -0500

    fix mutbuild

--- dockerjobs.ps1
+++ dockerjobs.ps1
@@ -3,7 +3,7 @@ if ($env:Dockerjobs -eq "True") {
     Set-Location $PSScriptRoot
     #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
     docker buildx ./rm mybuilder
-    docker buildx create --use --name mybuilder --driver-opt network=host --buildkitd-flags '--allow-insecure-entitlement network.host'
+    docker buildx create --use --name mybuilder #--driver-opt network=host --buildkitd-flags '--allow-insecure-entitlement network.host'
     docker buildx ls
     docker buildx inspect --bootstrap
     docker buildx build --platform=linux/amd64,linux/arm64/v8  -f dockerfile -t drjp81/plex:latest --progress plain --push .
